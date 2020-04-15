4ff1d8872f32af581f050c6f3abab95fa3b9f249
rubenjuarez1986 <rubenjuaeli@gmail.com>
2020-04-15 17:20:24 +0200

    problema solucionado

--- BetterAnimal/Program.cs
+++ BetterAnimal/Program.cs
@@ -16,7 +16,7 @@ namespace BetterAnimal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VentanaInicial());
+            Application.Run(new VentanaInicio());
         }
     }
 }
